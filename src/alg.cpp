6274d2a391509510a7c3ccf0d70c5e55b6127a6f
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-03-04 15:55:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,19 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
+#includ <cmath>
 
 
 double pown(double value, uint16_t n) {
-
+  pow(value, n)
 }
 
 uint64_t fact(uint16_t n) {
-
+  if(n==1){
+    return 1;
+  } else {
+    return n * fact3(n-1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
